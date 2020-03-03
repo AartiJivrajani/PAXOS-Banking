d65e2184c5ae316c2fdbeb7bb52452e58bc6c935
Aarti Jivrajani <aartij17@gmail.com>
2020-03-02 21:32:43 -0800

    fix timer logic, add final response sending to the client which requested the transaction

--- server/consensus/paxos.go
+++ server/consensus/paxos.go
@@ -13,8 +13,9 @@ import (
 
 // apologies for committing this blunder :P
 var (
-	acceptedMsgTimeout bool
-	timerStarted       bool
+	acceptedMsgTimeout = false
+	timerStarted       = false
+	clientTxn          *common.TransferTxn
 )
 
 // SendPrepare sends the prepare messages to all the other clients of the ecosystem
@@ -35,14 +36,14 @@ func (server *Server) getElected() {
 	// send a prepare message to all the servers
 	msg := &common.Message{
 		FromId: server.Id,
-		Type:   common.PREPARE_MESSAGE,
+		Type:   common.ELECTION_PREPARE_MESSAGE,
 		ElectionMsg: &common.ElectionMessage{
-			Type:   common.PREPARE_MESSAGE,
+			Type:   common.ELECTION_PREPARE_MESSAGE,
 			Ballot: server.Ballot,
 		},
 	}
 	jMsg, _ := json.Marshal(msg)
-	server.broadcastMessages(jMsg, common.PREPARE_MESSAGE)
+	server.broadcastMessages(jMsg, common.ELECTION_PREPARE_MESSAGE)
 }
 
 func (server *Server) broadcastMessages(msg []byte, msgType string) {
@@ -58,18 +59,18 @@ func (server *Server) broadcastMessages(msg []byte, msgType string) {
 	}
 }
 
-func (server *Server) sendAcceptMessage(conn net.Conn) {
+func (server *Server) sendAcceptMessage() {
 	msg := &common.Message{
 		FromId:     server.Id,
-		Type:       common.ACCEPT_MESSAGE,
+		Type:       common.ELECTION_ACCEPT_MESSAGE,
 		TxnMessage: nil,
 		ElectionMsg: &common.ElectionMessage{
-			Type:   common.ACCEPT_MESSAGE,
+			Type:   common.ELECTION_ACCEPT_MESSAGE,
 			Ballot: nil,
 		},
 	}
 	jMsg, _ := json.Marshal(msg)
-	server.broadcastMessages(jMsg, common.ACCEPTED_MESSAGE)
+	server.broadcastMessages(jMsg, common.ELECTION_ACCEPT_MESSAGE)
 }
 
 func (server *Server) validateBallotNumber(reqBallotNum int) bool {
@@ -80,7 +81,7 @@ func (server *Server) validateBallotNumber(reqBallotNum int) bool {
 }
 
 func (server *Server) waitForAcceptedMessages() {
-	timer := time.NewTimer(5 * time.Second)
+	timer := time.NewTimer(10 * time.Second)
 	timerStarted = true
 	select {
 	case <-timer.C:
@@ -89,7 +90,7 @@ func (server *Server) waitForAcceptedMessages() {
 	}
 }
 
-func (server *Server) processPeerLocalLogs(conn net.Conn, logs []*common.AcceptedMessage) {
+func (server *Server) processPeerLocalLogs(logs []*common.AcceptedMessage) {
 	block := &common.Block{
 		SeqNum:       server.SeqNum + 1,
 		Transactions: list.New(),
@@ -117,6 +118,31 @@ func (server *Server) processPeerLocalLogs(conn net.Conn, logs []*common.Accepte
 	}
 	jMsg, _ := json.Marshal(msg)
 	server.broadcastMessages(jMsg, common.COMMIT_MESSAGE)
+
+	// PHEW! PAXOS IS DONE! finally, send a response to the client.
+	server.sendResponseToClientAfterPaxos()
+}
+
+// sendResponseToClientAfterPaxos checks if the client transaction can still be carried out.
+func (server *Server) sendResponseToClientAfterPaxos() {
+	var (
+		clientResponse *common.Response
+		jResp          []byte
+	)
+	if server.checkIfTxnPossible(clientTxn) {
+		server.execLocalTxn(clientTxn)
+		clientResponse = &common.Response{
+			MessageType: common.SERVER_TXN_COMPLETE,
+		}
+	} else {
+		clientResponse = &common.Response{
+			MessageType: common.INSUFFICIENT_FUNDS,
+		}
+	}
+	jResp, _ = json.Marshal(clientResponse)
+	_, _ = server.getClientConnection().Write(jResp)
+	// invalidate the clientTxn after sending response to the client.
+	clientTxn = nil
 }
 
 // processPrepareMessage allows the server to decide if it should
@@ -132,9 +158,9 @@ func (server *Server) processPrepareMessage(conn net.Conn, msg *common.Message) 
 		server.Ballot.BallotNum = msg.ElectionMsg.Ballot.BallotNum
 		ackMsg := common.Message{
 			FromId: server.Id,
-			Type:   common.ELECTION_ACK_MESSAGE,
+			Type:   common.ELECTION_PROMISE_MESSAGE,
 			ElectionMsg: &common.ElectionMessage{
-				Type:   common.ELECTION_ACK_MESSAGE,
+				Type:   common.ELECTION_PROMISE_MESSAGE,
 				Ballot: msg.ElectionMsg.Ballot,
 			},
 		}
@@ -153,7 +179,7 @@ func (server *Server) processPrepareMessage(conn net.Conn, msg *common.Message) 
 	}
 }
 
-func (server *Server) sendAllLocalLogs(conn net.Conn) {
+func (server *Server) sendAllLocalLogs(msg *common.Message) {
 	logs := utils.LogToArray(server.Log)
 	accMsg := &common.AcceptedMessage{
 		Txns: logs,
@@ -162,13 +188,13 @@ func (server *Server) sendAllLocalLogs(conn net.Conn) {
 		Ballot: server.Ballot,
 		//SeqNum: 0, // TODO: Do we need this?
 	}
-	msg := &common.Message{
+	commonMessage := &common.Message{
 		FromId:          server.Id,
-		Type:            common.ACCEPTED_MESSAGE,
+		Type:            common.ELECTION_ACCEPTED_MESSAGE,
 		AcceptedMessage: accMsg,
 	}
-	jMsg, _ := json.Marshal(msg)
-	_, _ = conn.Write(jMsg)
+	jMsg, _ := json.Marshal(commonMessage)
+	_, _ = server.ServerConn[msg.FromId].Write(jMsg)
 }
 
 func (server *Server) updateBlockchain(msg *common.BlockMessage) {
@@ -180,11 +206,15 @@ func (server *Server) updateBlockchain(msg *common.BlockMessage) {
 		SeqNum:       msg.SeqNum,
 		Transactions: l,
 	}
+	log.WithFields(log.Fields{
+		"chain": utils.GetBlockchainPrint(l),
+	}).Info("created block from new txns recvd")
 	server.Blockchain.PushBack(block)
 }
 
 // execPaxosRun initiates a PAXOS run and then adds the transaction to the local block chain
 func (server *Server) execPaxosRun(txn *common.TransferTxn) {
 	// 1. perform leader election
+	clientTxn = txn
 	server.getElected()
 }
